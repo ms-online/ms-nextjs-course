c0f836a7121754774d928b863f35b4e94c9e0cb9
ms-online <27732357@qq.com>
2021-05-07 19:24:31 +0800

    lesson-9

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,9 +2,17 @@ import { useRouter } from 'next/router'
 function ClientProjectsPage() {
   const router = useRouter()
   console.log(router.query)
+  function loadProjectHandler() {
+    //加载数据...
+    router.push({
+      pathname: '/clients/[id]/[clientProjectId]',
+      query: { id: 'summer', clientProjectId: 'projectA' },
+    })
+  }
   return (
     <div>
       <h1>这是指定用户所有作品集页面</h1>
+      <button onClick={loadProjectHandler}> 加载summer的A作品</button>
     </div>
   )
 }
