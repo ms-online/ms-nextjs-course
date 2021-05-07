2b9d4a830db98ec3688b71c1f0d7ffed2ddb787b
ms-online <27732357@qq.com>
2021-05-07 11:55:15 +0800

    lesson-3

---
+++ pages/portfolio/[projectId].js
@@ -0,0 +1,9 @@
+function PortfolioProjectPage() {
+  return (
+    <div>
+      <h1>这是作品详情页面</h1>
+    </div>
+  )
+}
+
+export default PortfolioProjectPage
---
+++ pages/portfolio/list.js
@@ -0,0 +1,9 @@
+function ListPage() {
+  return (
+    <div>
+      <h1>这是作品列表页面</h1>
+    </div>
+  )
+}
+
+export default ListPage
