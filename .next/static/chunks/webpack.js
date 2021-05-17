e301992c71733cd32dd9eef15d64bf7edc7c3147
ms-online <27732357@qq.com>
2021-05-17 15:12:33 +0800

    lesson-35

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "aa91eddfe408f96a24d5";
+/******/ 	var hotCurrentHash = "60e23a709af0f764f1bb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
+import { useRef } from 'react'
+
 function HomePage() {
+  const nameInputRef = useRef()
+  const emailInputRef = useRef()
+  const feedbackInputRef = useRef()
+
+  function submitFormHandler(event) {
+    event.preventDefault()
+
+    const enteredName = nameInputRef.current.value
+    const enteredEmail = emailInputRef.current.value
+    const feedback = feedbackInputRef.current.value
+  }
+
   return (
     <div>
       <h1>主页</h1>
+      <form onsubmit={submitFormHandler}>
+        <div>
+          <label htmlFor='name'>姓名：</label>
+          <input type='text' id='name' ref={nameInputRef} />
+        </div>
+        <div>
+          <label htmlFor='email'>邮箱：</label>
+          <input type='email' id='email' ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor='feedback'>你的反馈：</label>
+          <textarea id='feedback' rows='5' ref={feedbackInputRef}></textarea>
+        </div>
+        <button>提交反馈</button>
+      </form>
     </div>
   )
 }
