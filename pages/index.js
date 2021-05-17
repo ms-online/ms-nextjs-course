2b9ca47303ff54afe2de0f48860feba22943b55a
ms-online <27732357@qq.com>
2021-05-17 14:07:31 +0800

    lesson-33

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,9 @@
-import path from 'path'
-import fs from 'fs/promises'
-import Link from 'next/link'
-function HomePage(props) {
-  const { products } = props
-
+function HomePage() {
   return (
-    <ul>
-      {products.map((product) => (
-        <li key={product.id}>
-          <Link href={`/products/${product.id}`}>{product.title}</Link>
-        </li>
-      ))}
-    </ul>
-  )
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
 }
 
-export async function getStaticProps() {
-  console.log('再次生成页面...')
-  const filePath = path.join(process.cwd(), 'data', 'dummy-data.json')
-  const jsonData = await fs.readFile(filePath)
-  const data = JSON.parse(jsonData)
-  if (!data) {
-    return {
-      redirect: {
-        destination: '/no-data',
-      },
-    }
-  }
-  if (data.products.length === 0) {
-    return { notFound: true }
-  }
-
-  return {
-    props: {
-      products: data.products,
-    },
-    revalidate: 10,
-  }
-}
-
-export default HomePage
+export default HomePage;
