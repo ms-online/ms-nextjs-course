2c32e945dca4173de299830f366d45af893ef261
ms-online <27732357@qq.com>
2021-05-17 14:31:54 +0800

    lesson-34

---
+++ pages/api/feedback.js
@@ -0,0 +1,5 @@
+function handler(req, res) {
+  res.status(200).json({ message: '响应成功！' })
+}
+
+export default handler
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function HomePage() {
   return (
     <div>
-      <h1>The Home Page</h1>
+      <h1>主页</h1>
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
